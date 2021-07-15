8806abb1ebc986e3f62000c581545e90c50560e6
Justice Martin <jujmart12@gmail.com>
2021-07-15 13:30:38 -0400

    Added user login api route

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -6,6 +6,7 @@ const { User } = require("../../db/models");
 
 const router = express.Router();
 
+// Log in
 router.post(
 	"/",
 	asyncHandler(async (req, res, next) => {
@@ -28,5 +29,11 @@ router.post(
 		});
 	})
 );
+
+// Log out
+router.delete("/", (_req, res) => {
+	res.clearCookie("token");
+	return res.json({ message: "success" });
+});
 
 module.exports = router;
