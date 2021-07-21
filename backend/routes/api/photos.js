aa47d5bea2274b3d34e58356c6297684f9ae7e9b
Justice Martin <jujmart12@gmail.com>
2021-07-21 10:59:52 -0400

    finished functionality for posting a photo

--- backend/routes/api/photos.js
+++ backend/routes/api/photos.js
@@ -1,8 +1,21 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
+const { check } = require("express-validator");
+
 const { Image, User } = require("../../db/models");
+const { handleValidationErrors } = require("../../utils/validation");
 
 const router = express.Router();
+
+const validateImage = [
+	check("imageUrl")
+		.exists({ checkFalsy: true })
+		.withMessage("Please provide an image url."),
+	check("userId")
+		.exists({ checkFalsy: true })
+		.withMessage("Please make sure you are signed in as a valid user."),
+	handleValidationErrors,
+];
 
 router.get(
 	"/",
@@ -18,6 +31,21 @@ router.get(
 		const { userId } = req.params;
 		const user = await User.scope("postUser").findByPk(userId);
 		return res.json({ user });
+	})
+);
+
+router.post(
+	"/",
+	validateImage,
+	asyncHandler(async (req, res) => {
+		const { imageUrl, title, content, userId } = req.body;
+		const photo = await Image.create({
+			imageUrl,
+			title,
+			content,
+			userId,
+		});
+		res.json({ photo });
 	})
 );
 
