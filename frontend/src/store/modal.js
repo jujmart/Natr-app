0c64f13458013fd0e14c33e1880e5ec385003612
Justice Martin <jujmart12@gmail.com>
2021-07-22 17:23:50 -0400

    finished styling for images and added delete photo confirm modal

--- frontend/src/store/modal.js
+++ frontend/src/store/modal.js
@@ -1,5 +1,6 @@
 const SHOW_LOGIN = "/modal/SHOW_LOGIN";
 const SHOW_SIGNUP = "/modal/SHOW_SIGNUP";
+const SHOW_DELETE_CONFIRM = "/modal/SHOW_DELETE_CONFIRM";
 const CLOSE = "/modal/CLOSE";
 
 export function setShowLogin() {
@@ -10,22 +11,28 @@ export function setShowSignup() {
 	return { type: SHOW_SIGNUP };
 }
 
+export function setShowDeleteConfirm() {
+	return { type: SHOW_DELETE_CONFIRM };
+}
+
 export function setClose() {
 	return { type: CLOSE };
 }
 
 export default function modalReducer(
-	state = { login: null, signup: null },
+	state = { login: null, signup: null, delete: null },
 	action
 ) {
 	Object.freeze(state);
 	switch (action.type) {
 		case SHOW_LOGIN:
-			return { login: true, signup: null };
+			return { login: true, signup: null, delete: null };
 		case SHOW_SIGNUP:
-			return { login: null, signup: true };
+			return { login: null, signup: true, delete: null };
+		case SHOW_DELETE_CONFIRM:
+			return { login: null, signup: null, delete: true };
 		case CLOSE:
-			return { login: null, signup: null };
+			return { login: null, signup: null, delete: null };
 		default:
 			return state;
 	}
