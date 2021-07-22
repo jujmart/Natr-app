94c8bcec6a093364eecc23309936241f1fd58c13
Justice Martin <jujmart12@gmail.com>
2021-07-22 10:52:26 -0400

    added model and migration for comments table

---
+++ backend/db/models/comment.js
@@ -0,0 +1,18 @@
+"use strict";
+module.exports = (sequelize, DataTypes) => {
+	const Comment = sequelize.define(
+		"Comment",
+		{
+			userId: { type: DataTypes.INTEGER, allowNull: false },
+			imageId: { type: DataTypes.INTEGER, allowNull: false },
+			comment: { type: DataTypes.TEXT, allowNull: false },
+		},
+		{}
+	);
+	Comment.associate = function (models) {
+		// associations can be defined here
+		Comment.belongsTo(models.User, { foreignKey: "userId" });
+		Comment.belongsTo(models.Image, { foreignKey: "imageId" });
+	};
+	return Comment;
+};
--- backend/db/models/image.js
+++ backend/db/models/image.js
@@ -17,6 +17,11 @@ module.exports = (sequelize, DataTypes) => {
 	Image.associate = function (models) {
 		// associations can be defined here
 		Image.belongsTo(models.User, { foreignKey: "userId" });
+		Image.hasMany(models.Comment, {
+			foreignKey: "imageId",
+			onDelete: "CASCADE",
+			hooks: true,
+		});
 	};
 	return Image;
 };
